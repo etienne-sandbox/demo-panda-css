f3bae4a55886054a01364b396016be7f46b90ba5
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-09-14 21:06:48 +0200

    Setup panda

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,11 @@
+import { css } from "../../styled-system/css";
+
 export function App() {
   return (
     <div>
-      <h1>Vite + React</h1>
+      <h1 className={css({ fontSize: "4xl", color: "blue.800" })}>
+        Vite + React
+      </h1>
     </div>
   );
 }
